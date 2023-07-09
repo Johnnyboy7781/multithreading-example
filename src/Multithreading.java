0a55b7f9036993b1c4b9890383a01e248d6d4325
Jonathan McDonnell <mcdonnell.jonathan@yahoo.com>
2023-07-09 17:31:07 -0400

    Add exception handling

--- src/Multithreading.java
+++ src/Multithreading.java
@@ -1,19 +1,28 @@
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.Random;
 import java.util.concurrent.CountDownLatch;
 
 public class Multithreading {
     public static void main(String[] args) throws InterruptedException {
-        List<Integer> list = new ArrayList<>(Arrays.asList(new Integer[1_000]));
+
+        final List<Integer> list = new ArrayList<>( Collections.nCopies(99,  0) );
+        list.add(1);
 
         long start = System.currentTimeMillis();
 
         CountDownLatch completedThreadCounter = new CountDownLatch(list.size());
 
         list.forEach((num) -> {
-            new Thread(() -> lengthyValidation(num, completedThreadCounter)).start();
+            new Thread(() -> {
+                try {
+                    lengthyValidation(num, completedThreadCounter);
+                } catch (Exception e) {
+                    completedThreadCounter.countDown();
+                    throw new RuntimeException(e);
+                }
+            }).start();
         });
 
         completedThreadCounter.await();
@@ -21,9 +30,13 @@ public class Multithreading {
         System.out.println("Execution took about " + (System.currentTimeMillis() - start) + "ms");
     }
 
-    public static void lengthyValidation(Integer num, CountDownLatch completedThreadCounter) {
+    public static void lengthyValidation(Integer num, CountDownLatch completedThreadCounter) throws Exception {
         Random random = new Random();
         int currInt = random.nextInt(500) + 500;
+
+        if (num == 1) {
+            throw new Exception("WHOOPS");
+        }
 
         System.out.println("Num = " + num + " || Sleeping for " + currInt + "ms");
         try {
