c9c9dd57792600af943f76cd2e4a328a0adeeceb
Jonathan McDonnell <mcdonnell.jonathan@yahoo.com>
2023-07-09 21:28:01 -0400

    Add finally block, variable name changes

--- src/Multithreading.java
+++ src/Multithreading.java
@@ -7,42 +7,41 @@ import java.util.concurrent.CountDownLatch;
 public class Multithreading {
     public static void main(String[] args) throws InterruptedException {
 
-        final List<Integer> list = new ArrayList<>( Collections.nCopies(99,  0) );
-        list.add(1);
+        final List<Integer> importantDataList = new ArrayList<>( Collections.nCopies(350,  0) );
+//        importantDataList.set(50, 1);
 
-        long start = System.currentTimeMillis();
+        final long start = System.currentTimeMillis();
 
-        CountDownLatch completedThreadCounter = new CountDownLatch(list.size());
+        CountDownLatch completedThreadCounter = new CountDownLatch( importantDataList.size() );
 
-        list.forEach((num) -> {
-            new Thread(() -> {
-                try {
-                    lengthyValidation(num, completedThreadCounter);
-                } catch (Exception e) {
-                    completedThreadCounter.countDown();
-                    throw new RuntimeException(e);
-                }
-            }).start();
-        });
+        importantDataList.forEach((importantData) -> new Thread(() -> {
+            try {
+                lengthyValidation(importantData);
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            } finally {
+                completedThreadCounter.countDown();
+            }
+        }).start());
 
         completedThreadCounter.await();
 
-        System.out.println("Execution took about " + (System.currentTimeMillis() - start) + "ms");
+        System.out.println("Execution took about " + ( System.currentTimeMillis() - start ) + "ms");
     }
 
-    public static void lengthyValidation(Integer num, CountDownLatch completedThreadCounter) throws Exception {
+    public static void lengthyValidation(Integer data) throws Exception {
         Random random = new Random();
-        int currInt = random.nextInt(500) + 500;
+        int sleepLength = random.nextInt(500) + 500;
 
-        if (num == 1) {
-            throw new Exception("WHOOPS");
-        }
+//        if (data == 1) {
+//            throw new Exception("WHOOPS");
+//        }
 
-        System.out.println("Num = " + num + " || Sleeping for " + currInt + "ms");
+        System.out.println("Num = " + data + " || Sleeping for " + sleepLength + "ms");
+
         try {
-            Thread.sleep(currInt);
+            Thread.sleep( sleepLength );
         } catch (Exception ignored) {}
 
-        completedThreadCounter.countDown();
     }
 }
