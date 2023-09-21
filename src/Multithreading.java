5bbbc7fa04917323eb0ea9bab70678d25883f5fe
Jonathan McDonnell <jonathan.mcdonnell@kinsaleins.com>
2023-09-21 10:17:08 -0400

    Add comments

--- src/Multithreading.java
+++ src/Multithreading.java
@@ -7,13 +7,17 @@ import java.util.concurrent.CountDownLatch;
 public class Multithreading {
     public static void main(String[] args) throws InterruptedException {
 
-        final List<Integer> importantDataList = new ArrayList<>( Collections.nCopies(350,  0) );
-//        importantDataList.set(50, 1);
+        // List of 350 pieces of data than need validation
+        final List<Integer> importantDataList = new ArrayList<>();
+        for (int i = 1; i <= 350; i++) {
+            importantDataList.add(i);
+        }
 
         final long start = System.currentTimeMillis();
 
         CountDownLatch completedThreadCounter = new CountDownLatch( importantDataList.size() );
 
+        // Multi thread to validate all data in parallel
         importantDataList.forEach((importantData) -> new Thread(() -> {
             try {
                 lengthyValidation(importantData);
@@ -24,6 +28,7 @@ public class Multithreading {
             }
         }).start());
 
+        // Tell main thread to wait until all other threads are complete
         completedThreadCounter.await();
 
         System.out.println("Execution took about " + ( System.currentTimeMillis() - start ) + "ms");
@@ -33,12 +38,16 @@ public class Multithreading {
         Random random = new Random();
         int sleepLength = random.nextInt(500) + 500;
 
+        // Uncomment this line to see how if one thread throws an exception,
+        // the others will continue without issue, including the main thread
 //        if (data == 1) {
 //            throw new Exception("WHOOPS");
 //        }
 
+        // When this is logged, notice how there is no guarantee of order
         System.out.println("Num = " + data + " || Sleeping for " + sleepLength + "ms");
 
+        // Tell thread to sleep between 500-1000ms to simulate lengthy validation
         try {
             Thread.sleep( sleepLength );
         } catch (Exception ignored) {}
